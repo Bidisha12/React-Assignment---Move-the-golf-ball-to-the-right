94f7f613973f02f50147240630eebcf57ca47a48
bidisha12 <bidishapaul237@gmail.com>
2020-11-13 01:04:56 +0530

    final
    Signed-off-by: bidisha12 <bidishapaul237@gmail.com>

--- src/components/App.js
+++ src/components/App.js
@@ -11,10 +11,11 @@ class App extends Component {
         };
         this.renderChoice = this.renderBallOrButton.bind(this)
         this.buttonClickHandler = this.buttonClickHandler.bind(this)
+        this.arrowKeyPressed=this.arrowKeyPressed.bind(this);
     };
 
     buttonClickHandler() {
-   
+        this.setState({renderBall:true});
    }
     renderBallOrButton() {
 		if (this.state.renderBall) {
@@ -23,10 +24,25 @@ class App extends Component {
 		    return <button onClick={this.buttonClickHandler} >Click For One Ball</button>
 		}
     }
+    arrowKeyPressed(evt)
+    {
+        if(evt.keyCode === 39)
+        {
+            if(this.state.renderBall)
+            {
+                const newPosI=this.state.posi + 5;
+                this.setState({
+                    posi: newPosI ,
+                    ballPosition: {left: newPosI + "px"}
+                })
+            }
+            
+        }
+    }
 
     // bind ArrowRight keydown event
     componentDidMount() {
-      
+      document.addEventListener("keydown",this.arrowKeyPressed);
     }
 
     render() {
